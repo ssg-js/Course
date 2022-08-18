55bf5c96600d07f68c24aba05e15edae1a5b20e7
ssg-js <likemath1997@gmail.com>
2022-08-18 16:35:19 +0900

    05_Stack1_단지번호_백준미통과

--- 05_Stack1/BOJ_2606_virus/s1.py
+++ 05_Stack1/BOJ_2606_virus/s1.py
@@ -4,8 +4,27 @@ import sys
 
 sys.stdin = open('input.txt')
 
-computers = int(input())
-adjacent_com = int(input())
-graph = [[] for _ in range(computers+1)]
 
-for i in range(adjacent_com):
+def dfs(v):
+    global cnt
+    passed[v] = True
+
+    for next_v in graph[v]:
+        if not passed[next_v]:
+            cnt += 1
+            dfs(next_v)
+
+
+computers = int(input())
+lines = int(input())
+graph = [[] for _ in range(computers+1)]
+passed = [False] * (computers + 1)
+cnt = 0
+
+for i in range(lines):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+dfs(1)
+print(cnt)
---
+++ 05_Stack1/BOJ_2667_numbering/s1.py
@@ -0,0 +1,36 @@
+import sys
+
+sys.stdin = open('input.txt')
+
+
+def dfs(x, y):
+    global cnt
+    cnt += 1
+    visited[x][y] = True
+
+    for d in range(4):
+        fx = x + dx[d]
+        fy = y + dy[d]
+
+        if 0 <= fx < n and 0 <= fy < n and not visited[fx][fy] and plane[fx][fy] == 1:
+            dfs(fx, fy)
+
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+n = int(input())
+plane = [list(map(int, input())) for _ in range(n)]
+visited = [[False] * n for _ in range(n)]
+result = []
+
+for i in range(n):
+    for j in range(n):
+        if not visited[i][j] and plane[i][j] == 1:
+            cnt = 0
+            dfs(i, j)
+            result.append(cnt)
+
+sorted(result)
+print(len(result))
+print(*result, sep='\n')
