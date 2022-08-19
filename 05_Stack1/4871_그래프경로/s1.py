7a3be0ca4f57e5d1a3d18c7052848efa9f316129
ssg-js <likemath1997@gmail.com>
2022-08-19 13:31:23 +0900

    06_문제풀이1_그래프경로_ing

--- 05_Stack1/4871_그래프경로/s1.py
+++ 05_Stack1/4871_그래프경로/s1.py
@@ -2,13 +2,23 @@ import sys
 
 sys.stdin = open('input.txt')
 
-# a에서 b로 가는 dfs
-def dfs(a, b):
-    visited[a] = True
+
+# x에서 y로 가는 dfs
+def dfs(x, y):
+    visited[x] = True
+
+    for node in info[x]:
+        dfs(node, y)
+        if node == y:
+            return 1
+    return 0
+
 
 for t in range(1, int(input()) + 1):
+
     nodes, edges = map(int, input().split())
     info = [[] for _ in range(nodes+1)]
+
     for _ in range(edges):
         a, b = map(int, input().split())
         info[a].append(b)
@@ -16,3 +26,5 @@ for t in range(1, int(input()) + 1):
     start, arrive = map(int, input().split())
 
     visited = [False] * (nodes+1)
+
+    print(f'#{t} {dfs(a, b)}')
