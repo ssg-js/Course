62bc84ff0dd2bb33112defbcaee76737807b9e00
ssg-js <likemath1997@gmail.com>
2022-08-25 23:44:48 +0900

    Feat: 12_Queue_BFS

--- 03_Algorithm/12_Queue/BFS 구현/s1.py
+++ 03_Algorithm/12_Queue/BFS 구현/s1.py
@@ -1,5 +1,15 @@
 def bfs(n):
+    visited[n] = True
+    queue = [n]
+    print(n, end=' ')
 
+    while queue:
+        n = queue.pop(0)
+        for next_n in graph[n]:
+            if not visited[next_n]:
+                visited[next_n] = True
+                queue.append(next_n)
+                print(next_n, end=' ')
 
 
 node, edge = map(int, input().split())
@@ -8,7 +18,9 @@ graph = [[] for _ in range(node + 1)]
 
 for i in range(edge):
     graph[info[2*i]].append(info[2*i+1])
+    graph[info[2*i+1]].append(info[2*i])
 
-visited = []
-queue = []
+visited = [False] * (node + 1)
+
+bfs(1)
 
