a51d30c6cb1210e7c5cb38162b4160a9d0ebbc72
ssg-js <likemath1997@gmail.com>
2022-08-26 17:26:37 +0900

    Feat: IM대비 주사위 쌓기 완료

--- IM_대비/BOJ_2116_주사위쌓기/s1.py
+++ IM_대비/BOJ_2116_주사위쌓기/s1.py
@@ -4,22 +4,22 @@ n = int(input())
 
 dices = [list(map(int, input().split())) for _ in range(n)]
 first_dice = dices.pop(0)
+match = [5, 3, 4, 1, 2, 0]
+
 max_value = []
 for i in range(6):
     total = 0
     clone = first_dice[:]
     top = clone[i]
-    clone[i], clone[5 - i] = 0, 0
+    clone[i], clone[match[i]] = 0, 0
     total += max(clone)
     for dice in dices:
         for j, v in enumerate(dice):
             if v == top:
                 clone = dice[:]
-                top = clone[5-j]
-                print(v, clone[5 - j])
-                clone[j], clone[5 - j] = 0, 0
+                top = clone[match[j]]
+                clone[j], clone[match[j]] = 0, 0
                 total += max(clone)
                 break
     max_value.append(total)
-print(*max_value)
 print(max(max_value))
