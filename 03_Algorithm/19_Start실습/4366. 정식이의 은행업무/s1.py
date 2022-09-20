6ef738b95d1c07bd463464efed525be73e8208d2
ssg-js <likemath1997@gmail.com>
2022-09-20 17:54:23 +0900

    Feat: 4366번 문제 미해결

--- 03_Algorithm/19_Start실습/4366. 정식이의 은행업무/s1.py
+++ 03_Algorithm/19_Start실습/4366. 정식이의 은행업무/s1.py
@@ -1,6 +1,12 @@
 # 4366. 정식이의 은행업무
 
 
+def decimal(arr, k):                       # k진수를 10진수로
+    result = 0
+    for index in range(len(arr)):
+        result += k ** arr.pop()
+
+
 for t in range(1, 11):
     n = int(input())
     binary = list(map(int, input()))
@@ -8,14 +14,14 @@ for t in range(1, 11):
 
     ans = 0
     for i, bi in enumerate(binary):
-        temp_bi = (bi + 1) % 2            # binary의 한자리가 틀린경우
+        temp_bi = (bi + 1) % 2             # binary의 한자리가 틀린경우
         temp_binary = binary[:]
         temp_binary[i] = temp_bi
         if temp_binary == int(trinary):
             ans = temp_binary
             break
 
-        for j, tri in enumerate(trinary):      # trinary의 한자리가 틀린경우
+        for j, tri in enumerate(trinary):  # trinary의 한자리가 틀린경우
             for plus in range(2):
                 temp_tri = (int(tri) + plus) % 2
                 temp_trinary = int(trinary, 3)
