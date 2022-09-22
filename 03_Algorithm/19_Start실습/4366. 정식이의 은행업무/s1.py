73e50faf05165275a3dd567be5d1faafa873e622
ssg-js <likemath1997@gmail.com>
2022-09-22 17:49:56 +0900

    Feat: 4366번 문제 미해결

--- 03_Algorithm/19_Start실습/4366. 정식이의 은행업무/s1.py
+++ 03_Algorithm/19_Start실습/4366. 정식이의 은행업무/s1.py
@@ -4,7 +4,8 @@
 def decimal(arr, k):                       # k진수를 10진수로
     result = 0
     for index in range(len(arr)):
-        result += k ** arr.pop()
+        result += (k ** index) * arr.pop()
+    return result
 
 
 for t in range(1, 11):
@@ -16,19 +17,15 @@ for t in range(1, 11):
     for i, bi in enumerate(binary):
         temp_bi = (bi + 1) % 2             # binary의 한자리가 틀린경우
         temp_binary = binary[:]
-        temp_binary[i] = temp_bi
-        if temp_binary == int(trinary):
-            ans = temp_binary
-            break
+        temp_binary[i] = temp_bi            # 틀린 자리 수정
 
         for j, tri in enumerate(trinary):  # trinary의 한자리가 틀린경우
             for plus in range(2):
                 temp_tri = (int(tri) + plus) % 2
-                temp_trinary = int(trinary, 3)
-                digit = temp_trinary
-                temp_trinary += 3 * (bi_digit_num - j)
-                if int(binary) == int(temp_trinary):
-                    ans = int(temp_binary)
+                temp_trinary = trinary[:]
+                temp_trinary[j] = temp_tri
+                if decimal(temp_trinary[:], 3) == decimal(temp_binary[:], 2):  # 3진수와 일치하면 정답
+                    ans = decimal(temp_trinary[:], 3)
                     break
             if ans:
                 break
