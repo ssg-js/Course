f48a27b380158670aed24258734d0f03749b8bbe
ssg-js <likemath1997@gmail.com>
2022-09-23 10:27:13 +0900

    Feat: 4266번 문제 해결

--- 03_Algorithm/19_Start실습/4366. 정식이의 은행업무/s1.py
+++ 03_Algorithm/19_Start실습/4366. 정식이의 은행업무/s1.py
@@ -8,24 +8,25 @@ def decimal(arr, k):                       # k진수를 10진수로
     return result
 
 
-for t in range(1, 11):
-    n = int(input())
+for t in range(1, int(input()) + 1):
     binary = list(map(int, input()))
     trinary = list(map(int, input()))
 
     ans = 0
     for i, bi in enumerate(binary):
-        temp_bi = (bi + 1) % 2             # binary의 한자리가 틀린경우
+        temp_bi = (bi + 1) % 2             # binary의 한자리 바꾸기
         temp_binary = binary[:]
-        temp_binary[i] = temp_bi            # 틀린 자리 수정
+        temp_binary[i] = temp_bi           # 틀린 자리 수정
+        b = decimal(temp_binary[:], 2)
 
-        for j, tri in enumerate(trinary):  # trinary의 한자리가 틀린경우
-            for plus in range(2):
-                temp_tri = (int(tri) + plus) % 2
+        for j, tri in enumerate(trinary):  # trinary의 한자리 바꾸기
+            for plus in range(1, 3):
+                temp_tri = (tri + plus) % 3
                 temp_trinary = trinary[:]
                 temp_trinary[j] = temp_tri
-                if decimal(temp_trinary[:], 3) == decimal(temp_binary[:], 2):  # 3진수와 일치하면 정답
-                    ans = decimal(temp_trinary[:], 3)
+                a = decimal(temp_trinary[:], 3)
+                if a == b:  # 3진수와 일치하면 정답
+                    ans = a
                     break
             if ans:
                 break
