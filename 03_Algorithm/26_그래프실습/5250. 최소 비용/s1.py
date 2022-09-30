b730f548e0a2b53b9414e8f23af421667d1e98b1
ssg-js <likemath1997@gmail.com>
2022-09-30 17:15:08 +0900

    Feat: 5250번 문제 미해결

---
+++ 03_Algorithm/26_그래프실습/5250. 최소 비용/s1.py
@@ -0,0 +1,35 @@
+# 5250. 최소 비용\
+# 각 좌표별 연료 소비량을 리스트에 저장
+
+
+def dijkstra(x, y):
+    visited = [[False] * N for _ in range(N)]
+    visited[x][y] = True
+    distance[x][y] = 0
+
+    
+
+
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+for t in range(1, int(input()) + 1):
+    N = int(input())
+    heights = [list(map(int, input().split())) for _ in range(N)]
+    INF = 99999999
+    distance = [[INF] * N for _ in range(N)]
+
+    edges = [[] for _ in range(N)]
+
+    for i in range(N):
+        for j in range(N):
+            for d in range(4):
+                nx = i + dx[d]
+                ny = j + dy[d]
+                if 0 <= nx < N and 0 <= ny < N:
+                    if heights[nx][ny] - heights[i][j] <= 0:        # 연료 1
+                        fuel = 1
+                    else:
+                        fuel = heights[nx][ny] - heights[i][j]      # 연료 마니
+                    edges[i][j].append((nx, ny, fuel))
+
--- 03_Algorithm/26_그래프실습/BOJ_1976. 여행 가자 (서로소 집합)/s1.py
+++ 03_Algorithm/26_그래프실습/BOJ_1976. 여행 가자 (서로소 집합)/s1.py
@@ -16,18 +16,20 @@ travel = list(map(int, input().split()))
 parent = list(range(N + 1))                                     # [0, 1, 2, ..., N]
 
 for i in range(N):
-    for j in range(i, N):
+    for j in range(i, N):                                       # 대각선 위쪽만 받음
         if info[i][j] == 1:
             x_root, y_root = find_set(i + 1), find_set(j + 1)           # Find
 
             if x_root != y_root:                                # 서로소
                 if x_root < y_root:
-                    parent[y_root] = parent[x_root]
+                    parent[y_root] = x_root
                 else:
-                    parent[x_root] = parent[y_root]
+                    parent[x_root] = y_root
 
 result = 'YES'
+print(parent)
 root = find_set(travel[0])
+
 for i in travel:
     if root != find_set(i):
         result = 'NO'
