7a91c699333693c7cc47945c294f64eb7a3f3756
ssg-js <likemath1997@gmail.com>
2022-11-02 17:02:34 +0900

    Feat: 1102_practice

--- 06_Vue/1102/03_Vue/vue-cli/src/components/MyComponent.vue
+++ 06_Vue/1102/03_Vue/vue-cli/src/components/MyComponent.vue
@@ -2,7 +2,12 @@
   <div class="border">
     <h1>싸피 이거는 내가 만든 새로운 컴포넌트다!!!</h1>
     <!-- 3. 등록하기 -->
-    <MyComponentItem/>
+    <MyComponentItem 
+    static-props="Mycomponent에서 보낸 데이터" 
+    :dynamic-props="dynamicProps"
+    @give-me-ma-money="parentGetEvent"
+    @child-input="getDynamicData"
+    />
   </div>
 </template>
 
@@ -15,6 +20,24 @@ export default {
   // 2. 등록하기
   components: {
     MyComponentItem,
+  },
+  // dynamic props
+  data: function () {
+    return {
+      dynamicProps : "It's in data"
+    }
+  },
+  methods: {
+    parentGetEvent: function (childData) {
+      console.log('용돈 없어!')
+      console.log(childData)
+      // App.vue 까지 올리기
+      this.$emit('toApp', childData) 
+    },
+    getDynamicData: function(childInputData) {
+      console.log(`사용자가 입력한 값은 ${childInputData} 입니다`)
+      console.log(childInputData)
+    }
   }
 }
 </script>
--- 06_Vue/1102/03_Vue/vue-cli/src/components/MyComponentItem.vue
+++ 06_Vue/1102/03_Vue/vue-cli/src/components/MyComponentItem.vue
@@ -1,12 +1,33 @@
 <template>
   <div>
     <h3>MyComponent의 하위 컴포넌트</h3>
+    <p>{{ staticProps }}</p>
+    <p>{{ dynamicProps }}</p>
+    <button @click="childToParent">클릭!</button>
+    <input @keyup.enter="childInput" type="text" name="" id="" v-model="childInputData">
   </div>
 </template>
 
 <script>
 export default {
   name: 'MyComponentItem',
+  props: {
+    staticProps: String,
+    dynamicProps: String,
+  },
+  data: function () {
+    return {
+      childInputData: null,
+    }
+  },
+  methods: {
+    childToParent: function () {
+      this.$emit('give-me-ma-money', '나는 자식이 보낸 데이터이다')
+    },
+    childInput: function () {
+      this.$emit('child-input', this.childInputData)
+    },
+  }
 }
 </script>
 
