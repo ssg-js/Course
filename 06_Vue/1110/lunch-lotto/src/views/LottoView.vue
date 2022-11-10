c042c0692f00963283cb09e2b59c3c606f2eff1b
ssg-js <likemath1997@gmail.com>
2022-11-10 11:36:09 +0900

    Feat: lunch-lotto 완료

---
+++ 06_Vue/1110/lunch-lotto/src/views/LottoView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>{{ lunchmenu }} 먹고 Lotto 추첨</h1>
+    <button @click="makeLotto">Pick Numbers</button>
+    <p>{{ lottonumbers }}</p>
+    <button @click="toLunch">처음으로</button>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'LottoView',
+  data () {
+    return {
+      lottonumbers: null,
+      lunchmenu: null,
+    }
+  },
+  methods: {
+    makeLotto () {
+      const allnum = _.range(1, 46)
+      this.lottonumbers = _.sampleSize(allnum, 6)
+    },
+    toLunch() {
+      this.$router.push({ name: 'lunch' })
+    },
+  },
+  created() {
+    console.log(this.$route.params)
+    this.lunchmenu = this.$route.params.lunchmenu
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 06_Vue/1110/lunch-lotto/src/views/LunchView.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Lunch</h1>
+    <button @click="lunchMenu()">Pick Lunch</button>
+    <br>
+    <p>{{ lunchmenu }}</p>
+    <button @click="toLottoButton" v-if="lunchmenu">Lotto 뽑으러</button>
+  </div>
+</template>
+
+<script>
+import _ from 'lodash'
+
+export default {
+  name: 'LunchView',
+  data() {
+    return {
+      lunchmenu: null,
+    }
+  },
+  methods: {
+    lunchMenu() {
+      this.lunchmenu = _.sample(["국밥", "짜장면", "햄버거", "비빔국수"]);
+      console.log(this.lunchmenu)
+    },
+    toLottoButton() {
+      this.$router.push({ name: 'lotto', params: {lunchmenu: this.lunchmenu}})
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
