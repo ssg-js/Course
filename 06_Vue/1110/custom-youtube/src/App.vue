95055a10e22c667be55bab37291e39edbbde5d5a
ssg-js <likemath1997@gmail.com>
2022-11-10 17:52:48 +0900

    Feat: youtube 미완성

---
+++ 06_Vue/1110/custom-youtube/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import MainVideo from '@/components/MainVideo'
+import SearchBar from '@/components/SearchBar'
+import VideoList from '@/components/VideoList'
+
+export default {
+  name: 'App',
+  components: {
+    MainVideo,
+    SearchBar,
+    VideoList,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 06_Vue/1110/custom-youtube/src/components/VideoListItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'item'
+}
+</script>
+
+<style>
+
+</style>
